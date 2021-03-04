46f0d76bb417a425127a120e67f0628dc07df153
Pierce <piercepatrick2001@gmail.com>
2021-03-03 22:22:59 -0600

    wrote functions for book objectand adding book objects into library array

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// JS
+let myLibrary = ['book1', 'book2'];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(newBook) {
+    myLibrary.push(newBook);
+}
+
+function displayBooks() {
+    for (i=0; i< myLibrary.length; i++)
+    {
+        console.log(myLibrary[i]);
+    }
+}
+
+let narnia = new Book();
+addBookToLibrary(narnia);
+displayBooks();
