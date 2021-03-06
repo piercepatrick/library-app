817eb1080d004728c93b44a36c470d1364c64644
Pierce <piercepatrick2001@gmail.com>
2021-03-06 17:13:56 -0600

    wrote makeCard() function, styled each card

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-let myLibrary = ['book1', 'book2'];
+const container = document.querySelector('.container');
 
-function Book(title, author, pages) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-}
+let myLibrary = [];
+
+class Book {
+    constructor(
+      title = "Unknown",
+      author = "Unknown",
+      pages = "0",
+      isRead = "false"
+    ) {
+      this.title = title;
+      this.author = author;
+      this.pages = pages;
+      this.isRead = isRead;
+    }
+  }
 
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
@@ -13,10 +23,28 @@ function addBookToLibrary(newBook) {
 function displayBooks() {
     for (i=0; i< myLibrary.length; i++)
     {
-        console.log(myLibrary[i]);
+        makeCard();
     }
 }
 
-let narnia = new Book();
+function makeCard()
+{
+    let card = document.createElement('div');
+    let title = document.createElement('h1');
+    let author = document.createElement('h2');
+    let pages = document.createElement('p');
+    let isRead = document.createElement('p')
+    card.classList.add('card');
+    title.textContent = myLibrary[i].title;
+    author.textContent = myLibrary[i].author;
+    pages.textContent = myLibrary[i].pages;
+    isRead.textContent = myLibrary[i].isRead;
+    card.append(title);
+    card.append(author);
+    card.append(pages);
+    card.append(isRead);
+    container.append(card);
+}
+let narnia = new Book('narnia', 'author', 25,false);
 addBookToLibrary(narnia);
 displayBooks();
